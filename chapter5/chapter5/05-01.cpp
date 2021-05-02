76318c7c8645098deb95023fbc50420a73c0e458
NSSEI <chatswoodj@gmail.com>
2021-05-02 23:24:17 +0900

    add 05 - 01

---
+++ chapter5/chapter5/05-01.cpp
@@ -0,0 +1,84 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstring>
+
+namespace COMP_POS
+{
+	enum { CLERK, SENIOR, ASSIST, MANAGER };
+
+	void ShowPos(int pos)
+	{
+		switch (pos)
+		{
+		case CLERK:
+			std::cout << "사원" << std::endl;
+			break;
+		case SENIOR:
+			std::cout << "주임" << std::endl;
+			break;
+		case ASSIST:
+			std::cout << "대리" << std::endl;
+			break;
+		case MANAGER:
+			std::cout << "과장" << std::endl;
+		}
+	}
+}
+class NameCard
+{
+	char* name;
+	char* comp;
+	char* num;
+	int pos;
+
+public:
+	NameCard(char* myname, char* mycomp, char* mynum, int posi)
+	{
+		pos = posi;
+		int len1 = strlen(myname) + 1;
+		int len2 = strlen(mycomp) + 1;
+		int len3 = strlen(mynum) + 1;
+
+		name = new char[len1];
+		comp = new char[len2];
+		num = new char[len3];
+
+		strcpy(name, myname);
+		strcpy(comp, mycomp);
+		strcpy(num, mynum);
+	}
+	NameCard(NameCard &copy)
+		:pos(copy.pos)
+	{
+		name = new char[strlen(copy.name) + 1];
+		comp = new char[strlen(copy.comp) + 1];
+		num = new char[strlen(copy.num) + 1];
+
+		strcpy(name, copy.name);
+		strcpy(comp, copy.comp);
+		strcpy(num, copy.num);
+	}
+	void ShowNameCardInfo()
+	{
+		std::cout << name << std::endl;
+		std::cout << comp << std::endl;
+		std::cout << num << std::endl;
+		COMP_POS::ShowPos(pos);
+		std::cout << std::endl;
+	}
+	~NameCard()
+	{
+		delete[] name;
+		delete[] comp;
+		delete[] num;
+	}
+};
+
+int main()
+{
+	NameCard manClerk((char*)"Lee", (char*)"ABCEng", (char*)"010-1111-2222", COMP_POS::CLERK);
+	NameCard copy1 = manClerk;
+	copy1.ShowNameCardInfo();
+	manClerk.ShowNameCardInfo();
+}
