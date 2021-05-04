1ebd7e885a89a09268af83bc482925ae8c2a8c39
NSSEI <chatswoodj@gmail.com>
2021-05-04 23:10:11 +0900

    add 07 - 01

---
+++ chapter7/chapter7/07-01.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+class Car
+{
+private:
+	int gasolineGauge;
+public:
+	Car(int gas) : gasolineGauge(gas) {}
+	int GetGasGauge()
+	{
+		return gasolineGauge;
+	}
+};
+
+class HybridCar : public Car
+{
+private:
+	int electricGauge;
+public:
+	HybridCar(int gas, int elec)
+		: Car(gas), electricGauge(elec) {}
+	int GetElecGauge()
+	{
+		return electricGauge;
+	}
+};
+
+class HybridWaterCar : public HybridCar
+{
+private:
+	int waterGauge;
+public:
+	HybridWaterCar(int gas, int elec, int water)
+		:HybridCar(gas, elec), waterGauge(water) {}
+	void ShowCurrentGauge()
+	{
+		std::cout << "잔여 가솔린 : " << GetGasGauge() << std::endl;
+		std::cout << "잔여 전기량 : " << GetElecGauge() << std::endl;
+		std::cout << "잔여 워터량 : " << waterGauge << std::endl;
+	}
+};
+
+int main()
+{
+	HybridWaterCar hb(10, 20, 30);
+	hb.ShowCurrentGauge();
+}
