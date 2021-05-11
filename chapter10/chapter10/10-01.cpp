f3c87b5370ead26c07c98df399e2ee9a4c9ea746
gs-dp <chatswoodj@gmail.com>
2021-05-11 11:57:35 +0900

    add 10 - 01

---
+++ chapter10/chapter10/10-01.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+class Point
+{
+private:
+	int xpos, ypos;
+public:
+	Point(int x = 0, int y = 0) :xpos(x), ypos(y) {}
+	void ShowPosition() const
+	{
+		std::cout << "[" << xpos << ", " << ypos << "]" << std::endl;
+	}
+	Point& operator-=(const Point &pos2)
+	{
+		xpos -= pos2.xpos;
+		ypos -= pos2.ypos;
+		return *this;
+	}
+
+	Point& operator+=(const Point &pos2)
+	{
+		xpos += pos2.xpos;
+		ypos += pos2.ypos;
+		return *this;
+	}
+	friend Point operator-(const Point &pos1, const Point &pos2);
+	friend bool operator==(const Point &pos1, const Point &pos2);
+	friend bool operator!=(const Point &pos1, const Point &pos2);
+};
+
+Point operator-(const Point &pos1, const Point &pos2)
+{
+	Point pos(pos1.xpos - pos2.xpos, pos1.ypos - pos2.ypos);
+	return pos;
+}
+
+bool operator==(const Point &pos1, const Point &pos2)
+{
+	if (pos1.xpos == pos2.ypos && pos1.ypos == pos2.ypos)
+		return true;
+	else
+		return false;
+}
+
+bool operator!=(const Point &pos1, const Point &pos2)
+{
+	return !(pos1==pos2);
+}
+
+
+int main()
+{
+	Point pos1(3, 4);
+	Point pos2(10, 20);
+	Point pos3 = pos1 - pos2;
+
+	pos1.ShowPosition();
+	pos2.ShowPosition();
+	pos3.ShowPosition();
+
+	pos1 -= pos2;
+	pos1.ShowPosition();
+	pos2.ShowPosition();
+
+	pos1 += pos2;
+	pos1.ShowPosition();
+	pos2.ShowPosition();
+
+	std::cout << (pos1 == pos2) << std::endl;
+	std::cout << (pos1 != pos2) << std::endl;
+	return 0;
+}
